a66c144d290aa3c3e22d1c1ea8423e23f2ed2094
Meade Reamy <meade@reamy.com>
2023-07-31 10:33:53 -0400

    Feature: added themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,34 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        main: {
+
+"primary": "#FCFCFB",
+
+"secondary": "#030303",
+
+"accent": "#CA0000",
+
+"neutral": "#3F4242",
+
+"base-100": "#4b5563",
+
+"info": "#3abff8",
+
+"success": "#36d399",
+
+"warning": "#fbbd23",
+
+"error": "#f87272",
+        },
+      },
+    ],
+  },
 }
 
