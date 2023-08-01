ae4501edc1116c9c9bde9df695ceb534ad70b676
Brendan Law <lawbrendanm@gmail.com>
2023-08-01 14:00:00 -0400

    merging changes from master

---
+++ src/components/auth/SignUp.tsx
@@ -0,0 +1,51 @@
+import '../../App.css';
+import { useState } from "react";
+import  { Dispatch, SetStateAction } from "react";
+import CreateAccount from "./CreateAccount";
+import PersonalInfo from "./PersonalInfo";
+
+// type IAccountDetails = {
+//   username: string;
+//   password: string;
+//   name: string;
+//   email: string;
+//   age: number;
+//   sex: string;
+//   height: number;
+//   weight: number;
+//   experience: number;
+//   goals: string;
+//   equipment: boolean;
+//   trainer: boolean;
+//   profile_pic: string;
+//   online_status: boolean;
+// }
+
+const SignUp:React.FC = () => {
+  const [accountDetails, setAccountDetails] = useState<any>({});
+  let step = 1;
+
+  const handleCreateAccount = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+
+  };
+
+  return (
+    <div>
+      {
+        step === 1 ?
+          <CreateAccount
+            handleCreateAccount={handleCreateAccount}
+          />
+        : step == 2 ?
+          <PersonalInfo
+
+          />
+        : 'Hmmm'
+      }
+    </div>
+  );
+}
+
+export default SignUp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,56 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{html,tsx,jsx,}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FCFCFB",
+        secondary: "#030303",
+        accent: "#CA0000",
+        neutral: "#3F4242",
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        gymbuddy: {
+          primary: "#FCFCFB",
+          secondary: "#030303",
+          accent: "#CA0000",
+          neutral: "#3F4242",
+        }
+      },
+      "dark"
+    ],
+  },
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        main: {
+
+"primary": "#FCFCFB",
+
+"secondary": "#030303",
+
+"accent": "#CA0000",
+
+"neutral": "#3F4242",
+
+"base-100": "#4b5563",
+
+"info": "#3abff8",
+
+"success": "#36d399",
+
+"warning": "#fbbd23",
+
+"error": "#f87272",
+        },
+      },
+    ],
+  },
 }
 
