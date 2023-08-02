12e846bd55f8ffb8c2b8bc6af5255fed4f02a380
Michael Johnson <michael.johnson7c5@gmail.com>
2023-08-02 10:17:16 -0400

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
 import Auth from './components/auth/index';
+import Login from './components/auth/index'
 import Social from './components/social/index';
 import Notfound from './components/notfound';
+import Planner from './components/planner/Planner.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,8 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}/>
-        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/signup" element={<Auth />}/>
         <Route path="social" element={<Social />} />
+        <Route path="planner" element={<Planner />} />
 
         {/* New Routes Go ABOVE this line */}
         <Route path="*" element={<Notfound/>}/>
