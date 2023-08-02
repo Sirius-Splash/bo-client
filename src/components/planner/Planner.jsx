ede8bebc18c16511ef4032025698277c2f5758ff
Brendan Law <lawbrendanm@gmail.com>
2023-08-02 17:22:14 -0400

    functionality for adding exercises to a workout

--- src/components/planner/Planner.jsx
+++ src/components/planner/Planner.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Workouts from './subcomponents/Workouts.jsx';
-import Schedule from './subcomponents/Schedule.jsx';
 
 const Planner = () => {
   const [exercises, setExercises] = useState([]);
-  const [workouts, setWorkout] = useState(['Workout 1', 'Workout 2', 'Workout 3', 'Workout 4']);
+  const [workouts, setWorkouts] = useState([]);
+  const [workoutCount, setWorkoutCount] = useState(0);
   const [workoutType, setWorkoutType] = useState('SELECT WORKOUT TYPE');
 
   const getExercises = () => {
     axios.get(`https://api.api-ninjas.com/v1/exercises?type=${workoutType}`, {
       headers: {
-        'X-Api-Key': ""
+        'X-Api-Key': "ILx8qYo5tzMqnOLPwy8ofQ==3nBNBQN18CJH1Pxl"
       }
     })
     .then(response => {
@@ -29,12 +29,13 @@ const Planner = () => {
     getExercises();
   }, [workoutType]);
 
+  const addWorkout = () => {
+    console.log('Workout added!', workouts); // EDIT THIS TO SEND TO OTHER COMPONENT
+  }
+  console.log(workouts);
   return (
     <React.Fragment>
       <div className="flex items-center py-5">
-        {/* <div>
-          <Schedule workouts={workouts}/>
-        </div> */}
         <div className="py-5">
             <select className="select select-bordered w-full max-w-xs" value={workoutType} onChange={handleSelect}>
               <option disabled>SELECT WORKOUT TYPE</option>
@@ -46,8 +47,12 @@ const Planner = () => {
               <option value="stretching">STRETCHING</option>
               <option value="strongman">STRONGMAN</option>
             </select>
+            <button className="btn" onClick={addWorkout}>
+              Exercises
+              <div className="badge badge-secondary">{workoutCount}</div>
+            </button>
             <div className="flex items-center py-5">
-              <Workouts exercises={exercises}/>
+              <Workouts exercises={exercises} workouts={workouts} setWorkouts={setWorkouts} workoutCount={workoutCount} setWorkoutCount={setWorkoutCount}/>
             </div>
         </div>
       </div>
