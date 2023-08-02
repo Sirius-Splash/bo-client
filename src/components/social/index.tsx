f0f99ce3a96181652339fe8e8b80d1b30444d0ec
Michael Johnson <michael.johnson7c5@gmail.com>
2023-08-02 15:01:12 -0400

    rebased

--- src/components/social/index.tsx
+++ src/components/social/index.tsx
@@ -2,10 +2,15 @@ import PostForm from './PostForm.tsx'
 import Feed from "./feed";
 
 function Social() {
-  return (<>
+  return (
+  <div>
     <PostForm />
+<<<<<<< HEAD
     <Feed/>
   </>);
+=======
+  </div>);
+>>>>>>> 72ae357 (wip)
 }
 
 export default Social;
