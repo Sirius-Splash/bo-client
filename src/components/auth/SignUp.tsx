b8b2c828d2576cbdc60d7b51f85306bb7267f3a2
d-verhnjak <darko.verhnjak@gmail.com>
2023-08-02 12:32:08 -0400

    setting up adding users to db

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -24,6 +24,7 @@ const SignUp = () => {
   };
 
   const postSignup = (accountDetails, personalDetails) => {
+    console.log('Submitted')
     const completedDetails = {
       ...accountDetails,
       ...personalDetails
