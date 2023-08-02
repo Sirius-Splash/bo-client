5569b3cb8d2f15b1d376b687b36f2909b7aa61fa
Mead Solomon <mead93@gmail.com>
2023-08-02 09:28:00 -0500

    refator for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import './App.css'
-import Auth from './components/auth/index';
-import Social from './components/social/index';
-import Notfound from './components/notfound';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/auth/index";
-import DirectMessageSender from "./components/chat/DMSender";
-import DirectMessageList from "./components/chat/DMList";
+import Social from "./components/social/index";
+import Notfound from "./components/notfound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />}/>
-        <Route path="social" element={<Social />} />
+        <Route
+          path="/"
+          element={<Auth />}
+        />
+        <Route
+          path="social"
+          element={<Social />}
+        />
 
         {/* New Routes Go ABOVE this line */}
-        <Route path="*" element={<Notfound/>}/>
+        <Route
+          path="*"
+          element={<Notfound />}
+        />
       </Routes>
     </BrowserRouter>
-  )
-    <>
-      <Auth />
-      <DirectMessageList />
-      <DirectMessageSender />
-    </>
   );
 }
 
---
+++ src/components/chat/index.tsx
@@ -0,0 +1,9 @@
+import Chat from "./Chat";
+
+const DMs = (currentUserId, otherUserId) {
+  return (
+    <Chat currentUserId={currentUserId} otherUserId={otherUserId} />
+  )
+};
+
+export default DMs;
