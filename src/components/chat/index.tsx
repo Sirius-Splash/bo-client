dcddd90c0ad35d946830c93eeb138a3c7c4eb267
Meade Reamy <meade@reamy.com>
2023-08-03 17:09:40 -0400

    conditionally render gptChat`

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -2,32 +2,35 @@ import ChatSelectHeader from "./ChatSelect/header";
 import Chat from "./DM/Chat";
 import React from "react";
 import GPTChat from "./GPT/GPTChatParent";
+
 interface DMProps {
   currentUserId: number;
   otherUserId: number;
 }
 
 const DMs: React.FC<DMProps> = ({ currentUserId, otherUserId }) => {
-
   const [aichatid, setAiChatId] = React.useState(0);
   const [aiChatTrue, setAiChatTrue] = React.useState(false);
 
-
   React.useEffect(() => {
-
-  }
-  , [aiChatTrue, aichatid]);
-
-
+  }, [aiChatTrue, aichatid]);
 
   return (
     <div className="flex flex-col gap-1">
       <ChatSelectHeader setAiChatTrue={setAiChatTrue} setAiChatId={setAiChatId}></ChatSelectHeader>
 
-    <Chat
-      currentUserId={currentUserId}
-      otherUserId={otherUserId}
-    />
+      {aiChatTrue ? ( // If aiChatTrue is truthy, render GPTChat
+        <GPTChat
+          currentUserId={currentUserId}
+          chatid={aichatid}
+        />
+      ) : (
+        // Otherwise, render Chat
+        <Chat
+          currentUserId={currentUserId}
+          otherUserId={otherUserId}
+        />
+      )}
     </div>
   );
 };
