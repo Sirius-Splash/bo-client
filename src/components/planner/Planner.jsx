ccefbd0cab5bd2c664ff651ad2f1b21de0202de1
Brendan Law <lawbrendanm@gmail.com>
2023-08-03 11:13:20 -0400

    added some styling

--- src/components/planner/Planner.jsx
+++ src/components/planner/Planner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Workouts from './subcomponents/Workouts.jsx';
 
@@ -7,6 +7,7 @@ const Planner = () => {
   const [workouts, setWorkouts] = useState([]);
   const [workoutCount, setWorkoutCount] = useState(0);
   const [workoutType, setWorkoutType] = useState('SELECT WORKOUT TYPE');
+  const exModalRef = useRef(null);
 
   const getExercises = () => {
     axios.get(`https://api.api-ninjas.com/v1/exercises?type=${workoutType}`, {
@@ -31,8 +32,14 @@ const Planner = () => {
 
   const addWorkout = () => {
     console.log('Workout added!', workouts); // EDIT THIS TO SEND TO OTHER COMPONENT
+    setWorkouts([]);
+    setWorkoutCount(0);
   }
-  console.log(workouts);
+
+  const showExModal = () => {
+    if (exModalRef.current) exModalRef.current.showModal();
+  };
+
   return (
     <React.Fragment>
       <div className="flex items-center py-5">
@@ -47,10 +54,19 @@ const Planner = () => {
               <option value="stretching">STRETCHING</option>
               <option value="strongman">STRONGMAN</option>
             </select>
-            <button className="btn" onClick={addWorkout}>
+            <button className="btn" onClick={showExModal}>
               Exercises
               <div className="badge badge-secondary">{workoutCount}</div>
             </button>
+            <dialog ref={exModalRef} className="modal">
+              <form method="dialog" className="modal-box">
+                <p className="py-4">Example</p>
+                <div className="modal-action">
+                  <button className="btn">Close</button>
+                  <button className="btn" onClick={addWorkout}>Add Workout</button>
+                </div>
+              </form>
+            </dialog>
             <div className="flex items-center py-5">
               <Workouts exercises={exercises} workouts={workouts} setWorkouts={setWorkouts} workoutCount={workoutCount} setWorkoutCount={setWorkoutCount}/>
             </div>
--- src/components/planner/subcomponents/Exercises.jsx
+++ src/components/planner/subcomponents/Exercises.jsx
@@ -5,10 +5,10 @@ const Exercises = ({ exercises, setWorkouts, workoutCount, setWorkoutCount, work
 
   return (
     <React.Fragment>
-      <div style={{border: '2px solid black'}} className="flex items-left py-5">
+      <div className="flex items-left py-5 bg-secondary">
           <div className="pr-4 pl-2 w-2/3 text-left cursor-pointer font-bold">EXERCISE</div>
           <div className="pr-4 w-1/3 text-center font-bold">EQUIPMENT</div>
-          <div className="pr-4 w-1/4 text-center font-bold"> SETS</div>
+          <div className="pr-4 w-1/4 text-center font-bold">SETS</div>
           <div className="pr-2 w-1/4 text-center font-bold">REPS</div>
           {/* <div className="pr-2 w-6 text-center font-bold">ADD WORKOUT</div> */}
         </div>
